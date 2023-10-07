688556eee758a901ee5f85f652c0700f4193390f
Rexoo <herricux99@gmail.com>
2023-10-07 13:55:23 +0200

    upload files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello, Vue with TSX!');
+
+const changeMessage = () => {
+  message.value = 'Vue with TSX is awesome!';
+};
+</script>
